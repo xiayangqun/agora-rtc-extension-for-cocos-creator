ddc1d026d1f1def0abe2b95e706d4353e3066803
xiayangqun <xyq377x@163.com>
2026-06-05 20:51:59 +0800

    add license

--- tests/native-integration/Classes/Game.cpp
+++ tests/native-integration/Classes/Game.cpp
@@ -1,6 +1,4 @@
 /****************************************************************************
- Copyright (c) 2024 Agora.io
- 
  Integration test Game class - boots real Cocos engine with Agora JSB bindings.
  ****************************************************************************/
 #include "Game.h"
